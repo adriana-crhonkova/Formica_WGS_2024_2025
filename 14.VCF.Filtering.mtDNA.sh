9e2e2d9af3eab40e7c7970df141b22504afa5c25
adriana-crhonkova <ada.crhonkova@seznam.cz>
2026-08-04 12:59:54 +0200

    Update 14.VCF.Filtering.mtDNA.sh

--- 14.VCF.Filtering.mtDNA.sh
+++ 14.VCF.Filtering.mtDNA.sh
@@ -118,7 +118,7 @@ find /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/04.VCF.mtD
 31_filt.alignment_mtDNA.sh
 -------------------- START OF BASH JOB -----------------------------------------------------------------------------------------------------------------------------------
 #!/bin/bash 
-#SBATCH -J filt.alignment_mtDNA
+#SBATCH -J LMUF.alignment_mtDNA
 #SBATCH -o /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/04.VCF.mtDNA/logs/filt.alignment_mtDNA.out
 #SBATCH -e /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/04.VCF.mtDNA/logs/filt.alignment_mtDNA.err
 #SBATCH -t 00:30:00
@@ -183,4 +183,121 @@ scp -r re98maw@cool.hpc.lrz.de:/dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WG
 # File > Export Graphics
 # Statistics > Identical Sequences > Log to file? > Yes
 # Statistics > All stats > Log to file? > Yes
+
+
+_________________________________________________________________________________________________________________________________________________________________
+## Dataset 2
+
+# 33_vcf_filtering_mtDNA.sh
+------------- START OF THE SCRIPT ---------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash -l
+#SBATCH -J LMUF_mtDNA
+#SBATCH -o /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/04.VCF.mtDNA/logs/vcf_LMUF.out
+#SBATCH -e /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/04.VCF.mtDNA/logs/vcf_LMUF.err
+#SBATCH -t 00:15:00
+#SBATCH --get-user-env
+#SBATCH --clusters=biohpc_gen
+#SBATCH --partition=biohpc_gen_normal
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=6G
+#SBATCH --mail-user=ada.crhonkova@seznam.cz
+#SBATCH --mail-type=END,FAIL
+
+# set the script to stop immediately if any command fails or any part of a pipeline fails, preventing silent errors and corrupted results
+set -eo pipefail
+
+# Load environment
+eval "$(mamba shell hook --shell bash)"
+mamba activate /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/mamba_envs/bcftools.env
+
+
+#####
+##### 0. Pre-filtering -----------
+#####
+echo "Pre-Filtering"
+
+cd /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/04.VCF.mtDNA
+bcftools norm -f /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/Reference_Genome/Formica_hybrid_v1_wFhyb_Sapis.fa -m -both -O z -o /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/04.VCF.mtDNA/LMUF_samples_mtDNA_norm.vcf.gz /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/04.VCF.mtDNA/LMUF_samples_mtDNA_raw.vcf.gz
+
+bcftools filter --threads 1 -Oz -s+ --SnpGap 2 LMUF_samples_mtDNA_norm.vcf.gz > LMUF_samples_mtDNA_norm.SnpGap_2.vcf.gz && \
+
+bcftools filter --threads 1 -Oz -e 'TYPE!="snp"' -s NonSnp -m+ LMUF_samples_mtDNA_norm.SnpGap_2.vcf.gz > LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.vcf.gz && \
+
+bcftools filter --threads 1 -Oz -s Balance -m+ -i 'RPL>=1 && RPR>=1 && SAF>=1 && SAR>=1' LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.vcf.gz > LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.vcf.gz && \
+
+bcftools view --threads 1 -O z -f PASS LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.vcf.gz > LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.vcf.gz && \
+
+bcftools view --threads 1 LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.vcf.gz | vcfallelicprimitives --keep-info --keep-geno -t decomposed | sed '/^##/! s/|/\//g' | sed 's/\.:\.:\.:\.:\.:\.:\.:\./\.\/\.:\.:\.:\.:\.:\.:\.:\./g' | bcftools sort --temp-dir $TMPDIR --max-mem 4G -O z > LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.vcf.gz && \
+
+
+bcftools index -t LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.vcf.gz
+echo "bcftools index -n LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.vcf.gz"
+bcftools index -n LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.vcf.gz
+
+#####
+##### 1. SNP QUAL >= 30, biallelic -------------------------------------------------------------------
+#####
+echo "SNP QUAL >= 30, biallelic"
+
+bcftools filter --threads 1 --include 'QUAL >= 30 && TYPE="snp"' -Oz LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.vcf.gz > LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.vcf.gz
+echo "gunzip -c LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.vcf.gz | grep -vc '#'"
+gunzip -c LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.vcf.gz | grep -vc '#'
+
+bcftools view --threads 1 --min-alleles 2 --max-alleles 2 LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.vcf.gz -Oz > LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.biall.vcf.gz
+echo "gunzip -c LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.biall.vcf.gz | grep -vc '#'"
+gunzip -c LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.biall.vcf.gz | grep -vc '#'
+bcftools index -t LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.biall.vcf.gz
+
+
+#####
+##### 2. Correcting the header --------------------------------------------------
+#####
+echo "Correcting the header"
+
+# Extract header from VCF
+bcftools view -h LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.biall.vcf.gz > header.vcf
+
+# Fix fields
+perl -npe 's/<ID=AO,Number=A/<ID=AO,Number=\./' header.vcf | perl -npe 's/<ID=AD,Number=R/<ID=AD,Number=\./' | perl -npe 's/<ID=QA,Number=A/<ID=QA,Number=\./' | perl -npe 's/<ID=GL,Number=G/<ID=GL,Number=\./' > header_AO_AD_QA_GL.vcf
+
+# Replace corrected header
+bcftools reheader -h header_AO_AD_QA_GL.vcf -o LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.biall.fixedHeader.vcf.gz LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.biall.vcf.gz
+
+bcftools index -t LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.biall.fixedHeader.vcf.gz
+bcftools index -n LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.biall.fixedHeader.vcf.gz
+
+echo "DONE"
+
+------------- END OF THE SCRIPT ---------------------------------------------------------------------------------------------------------------------------------------------
+sacct -M biohpc_gen -j 4052197 --format=JobID,Elapsed,MaxRSS,AllocCPUS,State,ExitCode
+# less wall time 
+
+____________________________________________________________________________________________________________________________________________________________________
+
+
+------------------- START OF INTERACTIVE JOB -----------------------------------------------------------------------------------------------------------------------------------
+salloc --clusters=biohpc_gen --partition=biohpc_gen_inter -t 00:30:00 --mem=2G
+mamba activate bcftools.env
+
+cd /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/04.VCF.mtDNA/stats
+awk '{print $1}' LMUF_samples_mtDNA_qc_stats.txt > samples.list.LMUF # remove the first row "sampleID" by hand
+mv samples.list.LMUF ../samples.list.LMUF
+
+
+cd /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/04.VCF.mtDNA
+
+# Run the loop using samtools faidx to isolate ONLY the mtDNA region
+while IFS= read -r s; do
+    samtools faidx /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/Reference_Genome/Formica_hybrid_v1_wFhyb_Sapis.fa mtDNA | \
+    bcftools consensus -s "$s" -I LMUF_samples_mtDNA_norm.SnpGap_2.NonSNP.Balance.PASS.decomposed.SNPQ30.biall.fixedHeader.vcf.gz > "${s}_mtDNA.fa"
+    
+    # Clean up the fasta header so it is just the sample name
+    sed -i "s/>.*/>${s}/" "${s}_mtDNA.fa"
+done < samples.list.LMUF
+
+mkdir indiv_samples_mtDNA
+mv *fa indiv_samples_mtDNA
+
+find /dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/04.VCF.mtDNA/indiv_samples_mtDNA -maxdepth 1 -type f | wc -l
+# 140 files
 
