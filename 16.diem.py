1f9f6a6d1c75171a0bd2df66792e4c5feed98f6f
adriana-crhonkova <ada.crhonkova@seznam.cz>
2026-09-03 14:46:51 +0200

    Update 16.diem.py

--- 16.diem.py
+++ 16.diem.py
@@ -219,15 +219,6 @@ diem.save_DiemType(dPol,polarizedDiemTypeOutFile)
 sacct -M biohpc_gen -j 4158564 --format=JobID,Elapsed,MaxRSS,AllocCPUS,State
 
 
-
-
-
-
-
-
-
-
-
-
+scp re98maw@cool.hpc.lrz.de:/dss/dsslegfs01/pn73qe/pn73qe-dss-0002/Formica_WGS/WGS_2024_2025/03.VCF/diem/output/LMUF_all_ru_pol_aq_lu_reduced1.diemtype .
 
 
